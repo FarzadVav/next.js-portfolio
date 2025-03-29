ce1942841913093fbda992f99956e167aa1d6258
FarzadVav <farzad.vav.work@gmail.com>
2025-03-29 10:59:27 -0700

    add: vazir fonts

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,19 @@ import Button from "root/components/ui/Button";
 const HomePage = () => {
   return (
     <>
-      <p>خانه</p>
-      <Button>Sign up</Button>
-      <Button variant="ghost" color="danger" size="sm" rounded="full">
-        Login
-      </Button>
-      <Button variant="soft" color="success" size="lg">
-        Logout
-      </Button>
+      <h1 className="vazir-black text-5xl">Farzad vav</h1>
+      <h2 className="vazir-bold text-2xl">Farzad vav</h2>
+      <h3>Farzad vav</h3>
+
+      <div className="flex gap-3">
+        <Button className="mt-12">Sign up</Button>
+        <Button variant="ghost" color="danger" size="sm" rounded="full">
+          Login
+        </Button>
+        <Button variant="soft" color="success" size="lg">
+          Logout
+        </Button>
+      </div>
     </>
   );
 };
