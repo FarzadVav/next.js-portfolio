9187d29d26fff4e1634c6168a6ce55ac60d77101
FarzadVav <farzad.vav.work@gmail.com>
2025-04-03 13:30:55 +0330

    fix: Footer component

--- components/templates/Footer.tsx
+++ components/templates/Footer.tsx
@@ -1,25 +1,31 @@
 import Link from "next/link";
+
 import Flex from "../ui/Flex";
+import Space from "../ui/Space";
 import Button from "../ui/Button";
 
 const Footer = () => {
   return (
-    <footer className="container py-10 border-t border-foreground/10 mt-10">
-      <Flex justify="center">
-        <Link href={"/"}>
-          <Button shape="ghost">لینک</Button>
-        </Link>
-        <Link href={"/"}>
-          <Button shape="ghost">لینک</Button>
-        </Link>
-        <Link href={"/"}>
-          <Button shape="ghost">لینک</Button>
-        </Link>
-        <Link href={"/"}>
-          <Button shape="ghost">لینک</Button>
-        </Link>
-      </Flex>
-    </footer>
+    <>
+      <Space />
+
+      <footer className="container py-10 border-t border-foreground/10">
+        <Flex justify="center">
+          <Link href={"/"}>
+            <Button shape="ghost">لینک</Button>
+          </Link>
+          <Link href={"/"}>
+            <Button shape="ghost">لینک</Button>
+          </Link>
+          <Link href={"/"}>
+            <Button shape="ghost">لینک</Button>
+          </Link>
+          <Link href={"/"}>
+            <Button shape="ghost">لینک</Button>
+          </Link>
+        </Flex>
+      </footer>
+    </>
   );
 };
 
