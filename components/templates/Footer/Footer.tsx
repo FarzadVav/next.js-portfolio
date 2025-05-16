0f79d183e0b5927df512756f07bed0cf884ede04
FarzadVav <farzad.vav.work@gmail.com>
2025-05-16 20:11:37 +0330

    fix: Footer component

--- components/templates/Footer/Footer.tsx
+++ components/templates/Footer/Footer.tsx
@@ -1,27 +1,31 @@
 import Link from "next/link";
+import { MailIcon, PhoneIcon } from "lucide-react";
 
-import Flex from "../../ui/Flex/Index";
-import Space from "../../ui/Space/Index";
-import Button from "../../ui/Button/Index";
+import Flex from "root/components/ui/Flex/Index";
+import Space from "root/components/ui/Space/Index";
+import Button from "root/components/ui/Button/Index";
+import Paragraph from "root/components/ui/Paragraph/Index";
 
 const Footer = () => {
   return (
     <>
       <Space />
-
       <footer className="container py-10 border-t border-foreground/10">
-        <Flex justify="center">
-          <Link href={"/"}>
-            <Button shape="ghost">لینک</Button>
+        <Flex>
+          <Paragraph className="text-center" size="sm">
+            کليه حقوق و مالکیت محتوای اين سایت متعلق به وب تیونرز می باشد ©
+          </Paragraph>
+          <Link className="mr-auto" href={"mailto:farzad.vav.work@gmail"}>
+            <Button shape="ghost">
+              <span>farzad.vav.work@gmail.com</span>
+              <MailIcon className="compatible-icon-size" />
+            </Button>
           </Link>
-          <Link href={"/"}>
-            <Button shape="ghost">لینک</Button>
-          </Link>
-          <Link href={"/"}>
-            <Button shape="ghost">لینک</Button>
-          </Link>
-          <Link href={"/"}>
-            <Button shape="ghost">لینک</Button>
+          <Link href={"tel:09389461065"}>
+            <Button shape="ghost">
+              <span dir="ltr">0938 0946 1065</span>
+              <PhoneIcon className="compatible-icon-size" />
+            </Button>
           </Link>
         </Flex>
       </footer>
