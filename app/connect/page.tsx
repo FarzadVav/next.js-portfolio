fba6f5bab330381d8e6322316f9ddfbe3850ca2e
FarzadVav <farzad.vav.work@gmail.com>
2025-05-29 23:44:13 +0330

    fix: connect page building erros

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -9,201 +9,164 @@ import {
   UserPenIcon,
 } from "lucide-react";
 
-import Box from "root/components/ui/Box/Index";
-import Flex from "root/components/ui/Flex/Index";
-import Space from "root/components/ui/Space/Index";
 import githubImg from "root/public/logos/github.svg";
 import { $mergeKeys } from "root/utils/general.utils";
 import youtubeImg from "root/public/logos/youtube.svg";
-import Heading from "root/components/ui/Heading/Index";
 import discordImg from "root/public/logos/discord.svg";
 import { ROUTES } from "root/constants/routes.constants";
 import telegramImg from "root/public/logos/telegram.svg";
 import linkedinImg from "root/public/logos/linkedin.svg";
-import Paragraph from "root/components/ui/Paragraph/Index";
 import instagramImg from "root/public/logos/instagram.svg";
 import PageHero from "root/components/modules/PageHero/Index";
 
 const ConnectPage = () => {
   return (
     <>
-      <Space />
-
       <PageHero
         src=""
         title="بیاید باهم پروژه های خفن رو پیاده کنیم"
         description="لورم از صنعت چاپ 🧑‍💻 و با استفاده لازم است و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده 🔥 شناخت فراوان جامعه و متخصصان ✌️ را می طلبد، تا با نرم افزارها ✅ شناخت بیشتری را برای طراحان رایانه ای..."
       />
 
-      <Space />
-      <div className="container grid grid-cols-3 gap-3">
-        <Box>
-          <Flex space="lg">
-            <Image src={telegramImg} alt="Web tunners telegram channel" width={64} height={64} />
-            <div>
-              <Heading>کانال تلگرام ما</Heading>
+      <div className="container grid grid-cols-3 mt-9">
+        <div className="f-align gap-6">
+          <Image src={telegramImg} alt="Web tunners telegram channel" width={64} height={64} />
+          <div>
+            <span className="text-xl font-vazir-bold">کانال تلگرام ما</span>
 
-              <Space size="sm" />
-              <Paragraph size="sm">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده...
-              </Paragraph>
+            <p className="text-sm mt-3">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده...
+            </p>
 
-              <Space size="sm" />
-              <Link className="btn btn-outline" href={"https://telegram.org"} target="_blank">
-                <span>مشاهده</span>
-                <ArrowUpLeftIcon className="compatible-icon" />
-              </Link>
-            </div>
-          </Flex>
-        </Box>
-        <Box>
-          <Flex space="lg">
-            <Image src={instagramImg} alt="Web tunners telegram channel" width={64} height={64} />
-            <div>
-              <Heading>کانال تلگرام ما</Heading>
+            <Link className="btn btn-outline mt-3" href={"https://telegram.org"} target="_blank">
+              <span>مشاهده</span>
+              <ArrowUpLeftIcon className="compatible-icon" />
+            </Link>
+          </div>
+        </div>
 
-              <Space size="sm" />
-              <Paragraph size="sm">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده...
-              </Paragraph>
+        <div className="f-align gap-6">
+          <Image src={instagramImg} alt="Web tunners telegram channel" width={64} height={64} />
+          <div>
+            <span className="text-xl font-vazir-bold">کانال تلگرام ما</span>
 
-              <Space size="sm" />
-              <Link className="btn btn-outline" href={"https://telegram.org"} target="_blank">
-                <span>مشاهده</span>
-                <ArrowUpLeftIcon className="compatible-icon" />
-              </Link>
-            </div>
-          </Flex>
-        </Box>
-        <Box>
-          <Flex space="lg">
-            <Image src={linkedinImg} alt="Web tunners telegram channel" width={64} height={64} />
-            <div>
-              <Heading>کانال تلگرام ما</Heading>
+            <p className="text-sm mt-3">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده...
+            </p>
 
-              <Space size="sm" />
-              <Paragraph size="sm">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده...
-              </Paragraph>
+            <Link className="btn btn-outline mt-3" href={"https://telegram.org"} target="_blank">
+              <span>مشاهده</span>
+              <ArrowUpLeftIcon className="compatible-icon" />
+            </Link>
+          </div>
+        </div>
 
-              <Space size="sm" />
-              <Link className="btn btn-outline" href={"https://telegram.org"} target="_blank">
-                <span>مشاهده</span>
-                <ArrowUpLeftIcon className="compatible-icon" />
-              </Link>
-            </div>
-          </Flex>
-        </Box>
-        <Box>
-          <Flex space="lg">
-            <Image src={discordImg} alt="Web tunners telegram channel" width={64} height={64} />
-            <div>
-              <Heading>کانال تلگرام ما</Heading>
+        <div className="f-align gap-6">
+          <Image src={linkedinImg} alt="Web tunners telegram channel" width={64} height={64} />
+          <div>
+            <span className="text-xl font-vazir-bold">کانال تلگرام ما</span>
 
-              <Space size="sm" />
-              <Paragraph size="sm">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده...
-              </Paragraph>
+            <p className="text-sm mt-3">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده...
+            </p>
 
-              <Space size="sm" />
-              <Link className="btn btn-outline" href={"https://telegram.org"} target="_blank">
-                <span>مشاهده</span>
-                <ArrowUpLeftIcon className="compatible-icon" />
-              </Link>
-            </div>
-          </Flex>
-        </Box>
-        <Box>
-          <Flex space="lg">
-            <Image src={youtubeImg} alt="Web tunners telegram channel" width={64} height={64} />
-            <div>
-              <Heading>کانال تلگرام ما</Heading>
+            <Link className="btn btn-outline mt-3" href={"https://telegram.org"} target="_blank">
+              <span>مشاهده</span>
+              <ArrowUpLeftIcon className="compatible-icon" />
+            </Link>
+          </div>
+        </div>
 
-              <Space size="sm" />
-              <Paragraph size="sm">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده...
-              </Paragraph>
+        <div className="f-align gap-6">
+          <Image src={discordImg} alt="Web tunners telegram channel" width={64} height={64} />
+          <div>
+            <span className="text-xl font-vazir-bold">کانال تلگرام ما</span>
 
-              <Space size="sm" />
-              <Link className="btn btn-outline" href={"https://telegram.org"} target="_blank">
-                <span>مشاهده</span>
-                <ArrowUpLeftIcon className="compatible-icon" />
-              </Link>
-            </div>
-          </Flex>
-        </Box>
-        <Box>
-          <Flex space="lg">
-            <Image src={githubImg} alt="Web tunners telegram channel" width={64} height={64} />
-            <div>
-              <Heading>کانال تلگرام ما</Heading>
+            <p className="text-sm mt-3">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده...
+            </p>
 
-              <Space size="sm" />
-              <Paragraph size="sm">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده...
-              </Paragraph>
+            <Link className="btn btn-outline mt-3" href={"https://telegram.org"} target="_blank">
+              <span>مشاهده</span>
+              <ArrowUpLeftIcon className="compatible-icon" />
+            </Link>
+          </div>
+        </div>
 
-              <Space size="sm" />
-              <Link className="btn btn-outline" href={"https://telegram.org"} target="_blank">
-                <span>مشاهده</span>
-                <ArrowUpLeftIcon className="compatible-icon" />
-              </Link>
-            </div>
-          </Flex>
-        </Box>
+        <div className="f-align gap-6">
+          <Image src={youtubeImg} alt="Web tunners telegram channel" width={64} height={64} />
+          <div>
+            <span className="text-xl font-vazir-bold">کانال تلگرام ما</span>
+
+            <p className="text-sm mt-3">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده...
+            </p>
+
+            <Link className="btn btn-outline mt-3" href={"https://telegram.org"} target="_blank">
+              <span>مشاهده</span>
+              <ArrowUpLeftIcon className="compatible-icon" />
+            </Link>
+          </div>
+        </div>
+
+        <div className="f-align gap-6">
+          <Image src={githubImg} alt="Web tunners telegram channel" width={64} height={64} />
+          <div>
+            <span className="text-xl font-vazir-bold">کانال تلگرام ما</span>
+
+            <p className="text-sm mt-3">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده...
+            </p>
+
+            <Link className="btn btn-outline mt-3" href={"https://telegram.org"} target="_blank">
+              <span>مشاهده</span>
+              <ArrowUpLeftIcon className="compatible-icon" />
+            </Link>
+          </div>
+        </div>
       </div>
 
-      <Space />
-      <div className="container">
-        <Flex justify="center">
+      <div className="container mt-9">
+        <div className="f-center">
           <SparklesIcon className="compatible-icon" />
-          <Heading className="text-center" size="lg">
-            <h2>ایونت های جامعه</h2>
-          </Heading>
+          <h2 className="text-2xl font-kalameh-bold">ایونت های جامعه</h2>
           <SparklesIcon className="compatible-icon -scale-x-100" />
-        </Flex>
+        </div>
       </div>
 
-      <Space size="normal" />
-      <div className="container grid grid-cols-2 gap-3">
+      <div className="container mt-6 grid grid-cols-2 gap-3">
         {Array.from(new Array(4)).map(() => (
-          <Box key={Math.random()}>
+          <div className="card card-effect" key={Math.random()}>
             <div className="w-full aspect-video bg-foreground/10 rounded-lg" />
 
-            <Space size="normal" />
-            <Heading font="vazir">
-              <h2>استفاده از تکنیک توسعه بر پایه دامنه</h2>
-            </Heading>
+            <h2 className="mt-6 text-xl font-vazir-bold">استفاده از تکنیک توسعه بر پایه دامنه</h2>
 
-            <Space size="sm" />
-            <Paragraph>
+            <p className="mt-3">
               لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
               گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای
               شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد{" "}
-            </Paragraph>
+            </p>
 
-            <Space size="sm" />
-            <Link className="btn btn-outline w-full" href={$mergeKeys("/", [ROUTES.articles, 1])}>
+            <Link
+              className="btn mt-3 btn-outline w-full"
+              href={$mergeKeys("/", [ROUTES.articles, 1])}
+            >
               <span>ثبت نام</span>
               <UserPenIcon className="compatible-icon" />
             </Link>
-          </Box>
+          </div>
         ))}
       </div>
 
-      <Space />
-      <div className="container">
-        <Flex justify="center">
+      <div className="container mt-9">
+        <div className="f-center">
           <SparklesIcon className="compatible-icon" />
-          <Heading className="text-center" size="lg">
-            <h2>هر درخواستی از ما دارید بنویسید!</h2>
-          </Heading>
+          <h2 className="text-2xl font-vazir-bold">هر درخواستی از ما دارید بنویسید!</h2>
           <SparklesIcon className="compatible-icon -scale-x-100" />
-        </Flex>
+        </div>
       </div>
 
-      <Space />
-      <Paragraph className="container text-justify">
+      <p className="container mt-9 text-justify">
         لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک
         است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
         تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی
@@ -212,10 +175,9 @@ const ConnectPage = () => {
         فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و
         شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات
         پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
-      </Paragraph>
+      </p>
 
-      <Space />
-      <Flex className="container">
+      <div className="f-align gap-3 container">
         <label className="input input-outline w-1/2">
           <input className="input-field" placeholder="موضوع تیکت" name="connect_topic" />
           <ChevronDownIcon className="compatible-icon" />
@@ -223,9 +185,8 @@ const ConnectPage = () => {
         <label className="input input-outline w-1/2" dir="ltr">
           <input className="input-field" placeholder="09** *** ****" name="connect_phone" />
         </label>
-      </Flex>
-      <Space size="sm" />
-      <div className="container">
+      </div>
+      <div className="container mt-3">
         <label className="input input-outline input-textarea">
           <textarea
             name="connect_text"
@@ -233,13 +194,11 @@ const ConnectPage = () => {
             placeholder="متن تان را شرح دهید..."
           />
         </label>
-        <Space size="sm" />
-        <button className="btn btn-outline">
+        <button className="btn btn-outline mt-3">
           <span>فایل ضمیمه (اختیاری)</span>
           <FileIcon className="compatible-icon" />
         </button>
-        <Space size="sm" />
-        <button className="btn btn-fill mx-auto">
+        <button className="btn btn-fill mx-auto mt-3">
           <span>ثبت تیکت</span>
           <CheckIcon className="compatible-icon" />
         </button>
