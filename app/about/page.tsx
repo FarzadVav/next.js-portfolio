75d8894cf5ce96e779f48552c937098b238545d5
FarzadVav <farzad.vav.work@gmail.com>
2025-05-30 13:24:55 +0330

    fix: about page design

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,10 @@
 import Link from "next/link";
-import {
-  AppleIcon,
-  ArrowUpLeft,
-  ArrowUpLeftIcon,
-  BiohazardIcon,
-  CheckIcon,
-  PlusIcon,
-  SparklesIcon,
-} from "lucide-react";
+import { AppleIcon, ArrowUpLeft, ArrowUpLeftIcon, CheckIcon, PlusIcon } from "lucide-react";
 
 import ScrollX from "root/components/ui/ScrollX/Index";
 import Accordion from "root/components/ui/Accordion/Index";
 import PageHero from "root/components/modules/PageHero/Index";
+import PageTitle from "root/components/modules/PageTitle/Index";
 import AccordionHeader from "root/components/ui/Accordion/Header";
 import ScrollXContainer from "root/components/ui/ScrollX/Container";
 import AccordionContent from "root/components/ui/Accordion/Content";
@@ -27,10 +20,10 @@ const AboutPage = () => {
       />
 
       <div className="f-align mt-9 container relative" dir="ltr">
-        <div className="absolute pointer-events-none top-0 bottom-0 right-0 w-1/3 bg-gradient-to-l from-background to-transparent" />
-        <div className="absolute pointer-events-none top-0 bottom-0 left-0 w-1/3 bg-gradient-to-r from-background to-transparent" />
+        <div className="absolute pointer-events-none top-0 bottom-0 right-0 w-1/3 bg-gradient-to-l from-background-1 to-transparent" />
+        <div className="absolute pointer-events-none top-0 bottom-0 left-0 w-1/3 bg-gradient-to-r from-background-1 to-transparent" />
 
-        <div className="f-align overflow-x-auto *:min-w-fit" style={{ scrollbarWidth: "none" }}>
+        <div className="f-align overflow-hidden no-scrollbar gap-3 *:min-w-fit">
           <div className="badge">
             <AppleIcon className="compatible-icon" />
             <span>Apple</span>
@@ -90,46 +83,42 @@ const AboutPage = () => {
         </div>
       </div>
 
-      <div className="container mt-9">
-        <video className="bg-foreground-5 card w-full aspect-video" />
+      <div className="container mt-9 card-effect">
+        <video className="bg-foreground-5 w-full card aspect-video" />
       </div>
 
-      <div className="container mt-9">
-        <div className="f-center">
-          <SparklesIcon className="compatible-icon" />
-          <h2 className="text-2xl text-center font-kalameh-bold">
-            ما چه کارهایی می‌توانیم انجام دهیم؟
-          </h2>
-          <SparklesIcon className="compatible-icon -scale-x-100" />
-        </div>
-        <ScrollX className="mt-3">
-          <ScrollXContainer>
-            {Array.from(new Array(7)).map(() => (
-              <div className="card card-effect w-1/3 min-w-1/3" key={Math.random()}>
-                <div className="w-2/3 mx-auto aspect-square rounded-lg bg-foreground-5" />
-                <p className="text-xl mt-6 text-center font-kalameh-bold">
-                  جذب {(3_637_000).toLocaleString("fa")} کاربر
-                </p>
-                <p className="text-center mt-3">
-                  ما در پیشرفته ترین سازمان های ایران با افراد زیبا کار کرده ایم...
-                </p>
-              </div>
-            ))}
-          </ScrollXContainer>
-        </ScrollX>
-      </div>
+      <PageTitle className="mt-9">
+        <h2>ما چه کارهایی می‌توانیم انجام دهیم؟</h2>
+      </PageTitle>
+      <ScrollX className="container mt-6">
+        <ScrollXContainer>
+          {Array.from(new Array(7)).map(() => (
+            <div className="card-effect w-2/5 min-w-2/5" key={Math.random()}>
+              <div className="w-2/3 mx-auto aspect-square rounded-lg bg-foreground-5" />
+              <p className="text-xl mt-6 text-center font-kalameh-bold">
+                جذب {(3_637_000).toLocaleString("fa")} کاربر
+              </p>
+              <p className="text-center mt-3">
+                ما در پیشرفته ترین سازمان های ایران با افراد زیبا کار کرده ایم...
+              </p>
+            </div>
+          ))}
+        </ScrollXContainer>
+      </ScrollX>
 
       <div className="mt-9">
         <CtaAnimation>
-          <div className="card card-effect">
-            <h3 className="text-xl font-vazir-black">خدمات ما راه موفقیت کسب و کار شماست!</h3>
+          <div className="card-effect">
+            <h3 className="text-2xl font-kalameh-bold">خدمات ما راه موفقیت کسب و کار شماست!</h3>
+
             <p className="mt-3">
               معمولا طراحان گرافیک برای صفحه‌آرایی، نخست از متن‌های آزمایشی و بی‌معنی استفاده
               می‌کنند تا صرفا به مشتری یا صاحب کار خود نشان دهند که صفحه طراحی یا صفحه بندی شده بعد
               از اینکه متن در آن قرار گیرد چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در
               نظر گرفته شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند
             </p>
-            <div className="f-align mt-3">
+
+            <div className="f-align gap-3 mt-6">
               <div className="badge">
                 <CheckIcon className="compatible-icon" />
                 <span>حسابداری</span>
@@ -151,7 +140,8 @@ const AboutPage = () => {
                 <span>مدیریت مشتری</span>
               </div>
             </div>
-            <Link href={"/"} className="btn btn-fill mt-3">
+
+            <Link href={"/"} className="btn-fill mt-6">
               <span>مشاهده خدمات</span>
               <ArrowUpLeftIcon className="compatible-icon" />
             </Link>
@@ -159,56 +149,43 @@ const AboutPage = () => {
         </CtaAnimation>
       </div>
 
-      <div className="container mt-9">
-        <div className="f-center">
-          <SparklesIcon className="compatible-icon" />
-          <h2 className="text-2xl font-vazir-bold">نمونه پروژه های ما</h2>
-          <SparklesIcon className="compatible-icon -scale-x-100" />
-        </div>
-      </div>
-      <div className="container mt-9 grid grid-cols-2">
+      <PageTitle className="mt-9">
+        <h4>نمونه پروژه های ما</h4>
+      </PageTitle>
+      <div className="container mt-6 grid grid-cols-2">
         {Array.from(new Array(7)).map(() => (
-          <div className="card card-effect" key={Math.random()}>
-            <div className="w-1/3 mx-auto f-center p-3 aspect-square border border-foreground rounded-full">
-              <BiohazardIcon className="size-full" />
-            </div>
+          <div className="card-effect" key={Math.random()}>
+            <div className="w-full aspect-video bg-foreground-5 rounded-lg" />
 
-            <h2 className="text-xl font-vazir-bold mt-3">استفاده از تکنیک توسعه بر پایه دامنه</h2>
-            <div className="badge mx-auto mt-1.5">مهندسی نرم افزار</div>
+            <h6 className="text-xl mt-3 font-vazir-bold">استفاده از توسعه بر پایه دامنه</h6>
 
-            <p className="text-center mt-3">
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از طراحان گرافیک است چاپگرها و متون بلکه
-              روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و
-              کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.
+            <div className="badge mt-3">مهندسی نرم افزار</div>
+
+            <p className="mt-3 line-clamp-3">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+              گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای
+              شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد
             </p>
 
-            <Link className="w-full mt-3" href={"https://melktoday.ir/ads"}>
-              <button className="w-full btn btn-outline">
-                <span>مشاهده</span>
-                <ArrowUpLeft className="compatible-icon" />
-              </button>
+            <Link className="mt-6 btn-outline" href={"https://github.com"}>
+              <span>بیشتر بخوانید</span>
+              <ArrowUpLeft className="compatible-icon" />
             </Link>
           </div>
         ))}
       </div>
 
-      <div className="container mt-6">
-        <div className="f-center">
-          <SparklesIcon className="compatible-icon" />
-          <h2 className="text-2xl font-vazir-bold">پاسخ های سوال هایتان</h2>
-          <SparklesIcon className="compatible-icon -scale-x-100" />
-        </div>
-      </div>
+      <PageTitle className="mt-9">
+        <h5>پاسخ های سوال هایتان</h5>
+      </PageTitle>
       <div className="container mt-6" id="FAQs">
         {Array.from(new Array(6)).map(() => (
           <Accordion key={Math.random()}>
             <AccordionHeader>
-              <h4 className="text-lg font-vazir-bold">
-                چگونه میتوانم یک سیستم کشینگ با اکسپرس و ریکت پیاده کنم؟
-              </h4>
+              <h6>چگونه میتوانم یک سیستم کشینگ با اکسپرس و ریکت پیاده کنم؟</h6>
             </AccordionHeader>
             <AccordionContent>
-              <p className="pt-3">
+              <p className="pt-6">
                 لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان
                 گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و
                 برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی
@@ -223,7 +200,7 @@ const AboutPage = () => {
           </Accordion>
         ))}
 
-        <button className="mx-auto mt-6 btn btn-soft">
+        <button className="mx-auto mt-6 btn-soft">
           <span>بیشتر</span>
           <PlusIcon className="compatible-icon" />
         </button>
