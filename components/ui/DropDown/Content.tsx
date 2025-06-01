e9930a7ee3228ac8e6ef11ef3d6ce2a426065b40
FarzadVav <farzad.vav.work@gmail.com>
2025-06-01 18:31:48 +0330

    fix: DropDownContent

--- components/ui/DropDown/Content.tsx
+++ components/ui/DropDown/Content.tsx
@@ -11,7 +11,7 @@ const DropDownContent: React.FC<DropDownContentProps> = ({ className, ...props }
   return (
     <div
       className={classMerge(
-        "bg-background min-w-full w-max ring-4 ring-foreground-5 absolute rounded-lg p-3 space-y-1.5 right-0 top-[calc(100%+4px)] transition-all z-20",
+        "bg-background-1 min-w-full w-max ring-4 ring-foreground-5 absolute rounded-lg p-3 space-y-1.5 right-0 top-[calc(100%+4px)] transition-all z-20",
         context.isOpen ? "" : "opacity-0 invisible -translate-y-1/12",
         className
       )}
