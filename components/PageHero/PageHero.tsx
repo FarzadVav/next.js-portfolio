30fe181f42abc408c6d66a00a0a0dde35bca4664
FarzadVav <farzad.vav.work@gmail.com>
2025-08-13 11:49:11 +0330

    add responsive styles to PageHero

--- components/PageHero/PageHero.tsx
+++ components/PageHero/PageHero.tsx
@@ -6,11 +6,11 @@ type PageHeroProps = Omit<React.HTMLAttributes<HTMLDivElement>, "children" | "ti
 
 const PageHero: React.FC<PageHeroProps> = ({ src, title, description, ...props }) => {
   return (
-    <div className="container f-align my-20 gap-6" {...props}>
+    <div className="container f-align my-20 gap-6 max-md:flex-col max-md:justify-center" {...props}>
       <div className="size-44 min-w-44 rounded-full bg-background-thin" title={src} />
 
-      <div>
-        <h1 className="text-3xl font-black">{title}</h1>
+      <div className="max-md:text-center">
+        <h1 className="title-lg font-black">{title}</h1>
         <p className="mt-3">{description}</p>
       </div>
     </div>
